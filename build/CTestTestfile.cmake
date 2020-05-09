47309c210d12af49ddb859fe16e8aa4434cda780
MrSanjeeva <you@example.com>
2020-05-09 16:35:56 +0100

    Intial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/groupproject/catkin_ws/src
+# Build directory: /home/groupproject/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(youbot_description)
+SUBDIRS(youbot_simulation/youbot_gazebo_control)
+SUBDIRS(youbot_simulation/youbot_gazebo_robot)
+SUBDIRS(youbot_simulation/youbot_gazebo_worlds)
+SUBDIRS(youbot_simulation/youbot_simulation)
